b62b85e3fc9b8ce0adbeee41a352f54a86cbe01d
Fahad Sarker <moajfahad@gmail.com>
2026-06-23 15:49:42 +0600

    update server ts file some chnages for getting error

--- src/server.ts
+++ src/server.ts
@@ -2,25 +2,17 @@ import app from './app';
 import {env} from './config/env';
 import { interactWithDB } from "./db/index";
 
+interactWithDB().catch((err) => {
+  console.error("Failed to connect to database", err);
+  process.exit(1);
+});
 
-const main = async () => {
-    try{
-     await interactWithDB();
 
-     
-     app.listen(env.PORT, () => {
-      console.log(`🚀 Server running on port ${env.PORT} http://localhost:${env.PORT}`);
-    });
-
-  }catch(err){
-    console.error("Failed To start Server", err);
-    process.exit(1);
-  
-
-  }
+if (env.NODE_ENV !== 'production') {
+  app.listen(env.PORT, () => {
+    console.log(`🚀 Server running on port ${env.PORT} http://localhost:${env.PORT}`);
+  });
 }
-
-main();
 
 
 export default app;
