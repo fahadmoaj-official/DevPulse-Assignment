ebc7f20f8e3db31e047a461a0d61048eddf7b801
Fahad Sarker <moajfahad@gmail.com>
2026-06-23 15:22:28 +0600

    Add cors and add live link in vercel & some chnage in pkg json for deploy

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,21 @@ import cookieParser from "cookie-parser";
 import type {Request, Response} from "express";
 import userRouter from "./modules/user/user.route";
 import issueRouter from "./modules/Issues/issues.route"
+import cors from "cors"
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+
+const corsOptions = {
+  origin: "https://devpulse-one-dun.vercel.app", 
+  methods: ["GET", "POST", "PUT","PATCH", "DELETE"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.get("/health", (req: Request, res: Response) => {
   
---
+++ tsup.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/server.ts"],
+
+  format: ["esm"],
+
+  target: "esnext",
+
+  outDir: "dist",
+  clean: true,
+    
+  bundle: true,
+  splitting: false,
+
+  sourcemap: true,
+
+  dts: true, 
+
+  banner: {
+    js: `"use strict";`,
+  },
+});
